127d83b0d3eb3ddafdedf95d423fa13cd3c9fedb
ttomczak3 <78294042+ttomczak3@users.noreply.github.com>
2021-12-02 18:51:10 -0600

    Add book upload

--- WebApplication1/adminbookinventory.aspx.cs
+++ WebApplication1/adminbookinventory.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +13,188 @@ namespace WebApplication1
 {
     public partial class adminbookinventory : System.Web.UI.Page
     {
+
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            fillAuthorPublisherValues();
+            GridView1.DataBind();
+
         }
+
+        // Go button
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Add button
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (checkIfBookExists())
+            {
+                Response.Write("<script>alert('Book already exists, try some other book ID');</script>");
+            }
+            else
+            {
+                addNewBook();
+            }
+
+        }
+
+        // Update button
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Delete button
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // User defined methods
+        void fillAuthorPublisherValues()
+        {
+
+            try
+            {
+
+                // Database connection
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                // Filling the author drop-down list from the database
+                SqlCommand cmd = new SqlCommand("SELECT author_name FROM author_master_tbl;", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                DropDownList3.DataSource = dt;
+                DropDownList3.DataValueField = "author_name";
+                DropDownList3.DataBind();
+
+                // Filling the publisher drop-down list from the database
+                cmd = new SqlCommand("SELECT publisher_name FROM publisher_master_tbl; ", con);
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+                DropDownList2.DataSource = dt;
+                DropDownList2.DataValueField = "publisher_name";
+                DropDownList2.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+
+        }
+
+        bool checkIfBookExists()
+        {
+
+            try
+            {
+
+                // Database connection
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                // Checking if the book exists
+                SqlCommand cmd = new SqlCommand("SELECT * from book_master_tbl WHERE book_id='" + TextBox1.Text.Trim() + "' OR book_name='" + TextBox2.Text.Trim() + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+
+        }
+
+        void addNewBook()
+        {
+
+            try
+            {
+
+                // Selecting multiple genres
+                string genres = "";
+                foreach (int i in ListBox1.GetSelectedIndices())
+                {
+                    genres = genres + ListBox1.Items[i] + ",";
+                }
+                
+                // Removing the last comma
+                genres = genres.Remove(genres.Length - 1);
+
+                // Saving the image file into the database
+                string filepath = "~/book_inventory/books1.png";
+                string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                FileUpload1.SaveAs(Server.MapPath("book_inventory/" + filename));
+                filepath = "~/book_inventory/" + filename;
+
+                // Database connection
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                // SQL insert query
+                SqlCommand cmd = new SqlCommand("INSERT INTO book_master_tbl(book_id,book_name,genre,author_name,publisher_name,publish_date,language,edition,book_cost,no_of_pages,book_description,actual_stock,current_stock,book_img_link) " +
+                    "values(@book_id,@book_name,@genre,@author_name,@publisher_name,@publish_date,@language,@edition,@book_cost,@no_of_pages,@book_description,@actual_stock,@current_stock,@book_img_link)", con);
+
+                // Inserting data into the database from the form
+                cmd.Parameters.AddWithValue("@book_id", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_name", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@genre", genres);
+                cmd.Parameters.AddWithValue("@author_name", DropDownList3.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@publisher_name", DropDownList2.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@publish_date", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@language", DropDownList1.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@edition", TextBox9.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_cost", TextBox10.Text.Trim());
+                cmd.Parameters.AddWithValue("@no_of_pages", TextBox11.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_description", TextBox6.Text.Trim());
+                cmd.Parameters.AddWithValue("@actual_stock", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@current_stock", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_img_link", filepath);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Book added successfully.');</script>");
+                GridView1.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+
+        }
+
     }
 }
